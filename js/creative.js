dd45efc0bb570705caf370275e79cad0af5171f0
Keshav Narula <NarulaKeshav13@gmail.com>
2016-01-05 19:57:24 -0800

    Added time

--- js/creative.js
+++ js/creative.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     
+    // INITIALIZING WOW.JS
+    new WOW().init();
+
     /* GETTING ELEMENTS IN HTML BY ID AND STORE THEM IN VARIABLE */
     var dateByUser = document.getElementById("date");
     var button = document.getElementById("calculate");
     var message = document.getElementById("message");
+    var hourMin = document.getElementById("hour-min");
     var reset = document.getElementById("reset");
 
     /* CONVERT THE TIME FROM MILLISECONDS TO DAYS */
@@ -17,7 +21,21 @@ $(document).ready(function() {
         return addCommas(diffInDays);
     }
 
-    /* ADDS COMMAS TO LARGE NUMBERS */
+    /* CONVERT THE TIME FROM MILLISECONDS TO HOURS */
+    function calculateHours() {
+        var diff = new Date().getTime() - new Date(dateByUser.value).getTime();
+        var diffInHr = Math.floor(diff / (1000 * 60 * 60));
+        return addCommas(diffInHr);
+    }
+
+    /* CONVERT THE TIME FROM MILLISECONDS TO MINUTES */
+    function calculateMinutes () {
+        var diff = new Date().getTime() - new Date(dateByUser.value).getTime();
+        var diffInMin = Math.floor(diff / (1000 * 60));
+        return addCommas(diffInMin);
+    }
+
+    /* ADDS COMMAS TO LARGE NUMBERS IF NECESSARY */
     function addCommas(num) {
         return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
@@ -32,20 +50,24 @@ $(document).ready(function() {
         else if(new Date().getFullYear() - new Date(dateByUser.value).getFullYear() >= 110) {
             var diff = calculateDays();
             message.innerHTML = "You're lying, but that's " + diff + " days.";
+            hourMin.innerHTML = "That's " + calculateHours() + " hours or " + calculateMinutes() + " minutes.";
         }
 
         // OTHERWISE, CALCULATE DAYS & MESSAGE = "YOU'VE LIVED ____ DAYS"
         else {
             var diff = calculateDays();
             if(diff == 1) message.innerHTML = "You've lived " + diff + " day.";
-            else message.innerHTML = "You've lived " + diff + " days.";
+            else {
+                message.innerHTML = "You've lived " + diff + " days.";
+                hourMin.innerHTML = calculateHours() + " hours or " + calculateMinutes() + " minutes.";
+            }
         }
     }
-  
+
     /* RESETS THE MESSAGE AND THE INPUT FIELD ON CLICK */
     reset.onclick = function() {
         dateByUser.value = "";
         message.innerHTML = "Days Lived";
     }
-
+    
 }); // CLOSE DOCUMENT READY
