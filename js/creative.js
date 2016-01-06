462ce4faf776c8b054c4ab39025167a6cfadb561
Keshav Narula <NarulaKeshav13@gmail.com>
2016-01-05 17:39:01 -0800

    Added commas

--- js/creative.js
+++ js/creative.js
@@ -9,11 +9,16 @@ $(document).ready(function() {
     // CALCULATES THE DAYS A PERSON HAS LIVED
     button.onclick = function() {
         if (dateByUser.value === "") { message.innerHTML = "Enter a date bro!"; }
+        else if(new Date().getFullYear() - new Date(dateByUser.value).getFullYear() >= 110) {
+            var difference = new Date().getTime() - new Date(dateByUser.value).getTime();
+            var mainDiff = Math.floor(difference / (1000 * 60 * 60 * 24));
+            message.innerHTML = "You're lying, but that's " + addCommas(mainDiff) + " days.";
+        }
         else {
             var difference = new Date().getTime() - new Date(dateByUser.value).getTime();
             var mainDiff = Math.floor(difference / (1000 * 60 * 60 * 24));
             if (mainDiff < 0) { message.innerHTML = "You ain't from the future lol"; }
-            else { message.innerHTML = "You've lived " + mainDiff + " days."; }
+            else { message.innerHTML = "You've lived " + addCommas(mainDiff) + " days."; }
         }
     }
   
@@ -21,5 +26,9 @@ $(document).ready(function() {
     reset.onclick = function() {
         dateByUser.value = "";
         message.innerHTML = "Days Lived";
+    }
+
+    function addCommas(num) {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 });
