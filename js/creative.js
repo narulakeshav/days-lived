6a59f2ced3af81697c39f0dcfd8ee4bd6d6e75d3
Keshav Narula <NarulaKeshav13@gmail.com>
2016-01-05 18:53:00 -0800

    Update the one day case

--- js/creative.js
+++ js/creative.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
         // OTHERWISE, CALCULATE DAYS & MESSAGE = "YOU'VE LIVED ____ DAYS"
         else {
             var diff = calculateDays();
-            if(diff === 1) message.innerHTML = "You've lived " + diff + " day.";
+            if(diff == 1) message.innerHTML = "You've lived " + diff + " day.";
             else message.innerHTML = "You've lived " + diff + " days.";
         }
     }
