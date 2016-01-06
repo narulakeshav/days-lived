55a9b6118726851d7ceb977d71197d11e0416a00
Keshav Narula <NarulaKeshav13@gmail.com>
2016-01-05 18:35:34 -0800

    Added useful comments

--- js/creative.js
+++ js/creative.js
@@ -1,35 +1,50 @@
 $(document).ready(function() {
     
-    // GETTING ELEMENTS IN HTML BY ID AND STORE THEM IN VARIABLE
+    /* GETTING ELEMENTS IN HTML BY ID AND STORE THEM IN VARIABLE */
     var dateByUser = document.getElementById("date");
     var button = document.getElementById("calculate");
     var message = document.getElementById("message");
     var reset = document.getElementById("reset");
 
-    // CALCULATES THE DAYS A PERSON HAS LIVED
+    /* CONVERT THE TIME FROM MILLISECONDS TO DAYS */
+    function calculateDays() {
+
+        // CALCULATE DIFFERENCE FROM TODAY TO THE INPUT DATE IN MILLISEONDS
+        var diff = new Date().getTime() - new Date(dateByUser.value).getTime();
+
+        // ROUND DOWN THE DAY AND DIVIDE THE DIFF (IN SECONDS) BY (1000 * 60 * 60 * 24)
+        var diffInDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+        return addCommas(diffInDays);
+    }
+
+    /* ADDS COMMAS TO LARGE NUMBERS */
+    function addCommas(num) {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    /* CALCULATES THE DAYS A PERSON HAS LIVED */
     button.onclick = function() {
+
+        // IF THE INPUT WAS EMPTY, MESSAGE = "ENTER A DATE BRO"
         if (dateByUser.value === "") { message.innerHTML = "Enter a date bro!"; }
+
+        //ELSE IF THE YEAR DIFFERENCE IS OVER 110, MESSAGE = "YOU'RE LYING ..."
         else if(new Date().getFullYear() - new Date(dateByUser.value).getFullYear() >= 110) {
-            var difference = new Date().getTime() - new Date(dateByUser.value).getTime();
-            var mainDiff = Math.floor(difference / (1000 * 60 * 60 * 24));
-            message.innerHTML = "You're lying, but that's " + addCommas(mainDiff) + " days.";
+            var diff = calculateDays();
+            message.innerHTML = "You're lying, but that's " + diff + " days.";
         }
+
+        // OTHERWISE, CALCULATE DAYS & MESSAGE = "YOU'VE LIVED ____ DAYS"
         else {
-            var difference = new Date().getTime() - new Date(dateByUser.value).getTime();
-            var mainDiff = Math.floor(difference / (1000 * 60 * 60 * 24));
-            if (mainDiff < 0) { message.innerHTML = "You ain't from the future lol"; }
-            else { message.innerHTML = "You've lived " + addCommas(mainDiff) + " days."; }
+            var diff = calculateDays();
+            message.innerHTML = "You've lived " + diff + " days."; 
         }
     }
   
-    // RESETS THE MESSAGE AND THE INPUT FIELD ON CLICK
+    /* RESETS THE MESSAGE AND THE INPUT FIELD ON CLICK */
     reset.onclick = function() {
         dateByUser.value = "";
         message.innerHTML = "Days Lived";
     }
 
-    // ADDS COMMAS TO LARGE NUMBERS
-    function addCommas(num) {
-        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
 });
